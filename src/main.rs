569a159732b6875ad4dc14e2d29e5973a1f51bf8
Rue Starsja <ruestarsja@gmail.com>
2025-10-21 20:59:52 -0400

    Comment out previous update logic, and draw a basic chess board to the window.

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ mod utils;
 use ggez::Context;
 use ggez::ContextBuilder;
 use ggez::GameResult;
+use ggez::conf;
 use ggez::event;
 use ggez::graphics;
-use text_io::read;
+// use text_io::read;
 
 use crate::components::chess_board::ChessBoard;
 use crate::utils::logs::init_logs;
@@ -17,7 +18,13 @@ use crate::utils::logs::log;
 
 fn main() {
     // create context and event loop
-    let ctx_and_event_loop_result = ContextBuilder::new("chess", "Rue Starsja").build();
+    let ctx_and_event_loop_result = ContextBuilder::new("chess", "Rue Starsja")
+        .window_mode(
+            conf::WindowMode::default()
+                .fullscreen_type(conf::FullscreenType::Windowed)
+                .resizable(true)
+        )
+        .build();
     let (mut ctx, event_loop) = match ctx_and_event_loop_result {
         Ok(_) => {
             ctx_and_event_loop_result.unwrap()
@@ -44,6 +51,7 @@ fn main() {
 struct Game {
     chess_board: ChessBoard,
     is_black_turn: bool,
+    screen_coords: graphics::Rect,
 }
 
 
@@ -57,6 +65,9 @@ impl Game {
         Self {
             chess_board: Default::default(),
             is_black_turn: false,
+            screen_coords: graphics::Rect::new(
+                0., 0., _ctx.gfx.drawable_size().0, _ctx.gfx.drawable_size().1
+            ),
         }
     }
 
@@ -66,51 +77,136 @@ impl Game {
 impl event::EventHandler for Game {
 
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        println!();
-        println!("{}", self.chess_board);
-        println!();
-        print!(" > ");
+        // println!();
+        // println!("{}", self.chess_board);
+        // println!();
+        // print!(" > ");
         
-        let input: String = read!("{}\n");
+        // let input: String = read!("{}\n");
 
-        if input == "exit" {
-            println!();
-            println!("Goodbye!");
-            println!();
-            _ctx.request_quit();
+        // if input == "exit" {
+        //     println!();
+        //     println!("Goodbye!");
+        //     println!();
+        //     _ctx.request_quit();
 
-        } else if input == "move" {
-            println!();
-            print!(" Starting File Label: ");
-            let start_file_label: String = read!("{}\n");
-            print!(" Starting Rank Label: ");
-            let start_rank_label: String = read!("{}\n");
-            print!(" Target File Label: ");
-            let target_file_label: String = read!("{}\n");
-            print!(" Target Rank Label: ");
-            let target_rank_label: String = read!("{}\n");
-            let success = self.chess_board.move_piece(
-                self.is_black_turn,
-                ChessBoard::get_rank(start_rank_label),
-                ChessBoard::get_file(start_file_label),
-                ChessBoard::get_rank(target_rank_label),
-                ChessBoard::get_file(target_file_label)
-            );
-            if success {
-                self.is_black_turn = !self.is_black_turn;
-            }
-        }
+        // } else if input == "move" {
+        //     println!();
+        //     print!(" Starting File Label: ");
+        //     let start_file_label: String = read!("{}\n");
+        //     print!(" Starting Rank Label: ");
+        //     let start_rank_label: String = read!("{}\n");
+        //     print!(" Target File Label: ");
+        //     let target_file_label: String = read!("{}\n");
+        //     print!(" Target Rank Label: ");
+        //     let target_rank_label: String = read!("{}\n");
+        //     let success = self.chess_board.move_piece(
+        //         self.is_black_turn,
+        //         ChessBoard::get_rank(start_rank_label),
+        //         ChessBoard::get_file(start_file_label),
+        //         ChessBoard::get_rank(target_rank_label),
+        //         ChessBoard::get_file(target_file_label)
+        //     );
+        //     if success {
+        //         self.is_black_turn = !self.is_black_turn;
+        //     }
+        // }
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let mut canvas = graphics::Canvas::from_frame(ctx, graphics::Color::WHITE);
-        // ...
+        // log("INFO", "Starting new draw.");
+        let mut canvas = graphics::Canvas::from_frame(ctx, graphics::Color::from_rgb(50, 45, 40));
+        canvas.set_screen_coordinates(self.screen_coords);
+
+        let board_side_len = if self.screen_coords.w < self.screen_coords.h { self.screen_coords.w } else { self.screen_coords.h };
+        let margin = (self.screen_coords.w - self.screen_coords.h).abs() / 2.0;
+        let board_x = if self.screen_coords.w < self.screen_coords.h { 0.0 } else { margin };
+        let board_y = if self.screen_coords.w < self.screen_coords.h { margin } else { 0.0 };
+
+        let mut meshes: Vec<graphics::Mesh> = vec![];
+        
+        meshes.push(
+            graphics::Mesh::new_rectangle(
+                ctx,
+                graphics::DrawMode::fill(),
+                graphics::Rect::new(
+                    board_x,
+                    board_y,
+                    board_side_len,
+                    board_side_len
+                ),
+                graphics::Color::from_rgb(80, 60, 20)
+            )?
+        );
+
+        meshes.push(
+            graphics::Mesh::new_rectangle(
+                ctx,
+                graphics::DrawMode::fill(),
+                graphics::Rect::new(
+                    board_x + 20.0,
+                    board_y + 20.0,
+                    board_side_len - 40.0,
+                    board_side_len - 40.0
+                ),
+                graphics::Color::from_rgb(140, 110, 65)
+            )?
+        );
+
+        let mut row = 0;
+        while row < 8 {
+            let mut col = 0;
+            while col < 8 {
+                if (row + col) % 2 == 0 {
+                    meshes.push(
+                        graphics::Mesh::new_rectangle(
+                            ctx,
+                            graphics::DrawMode::fill(),
+                            graphics::Rect::new(
+                                (board_x + 20.0) + (col as f32 * ((board_side_len - 40.0) / 8.0)),
+                                (board_y + 20.0) + (row as f32 * ((board_side_len - 40.0) / 8.0)),
+                                (board_side_len - 40.0) / 8.0,
+                                (board_side_len - 40.0) / 8.0
+                            ),
+                            graphics::Color::from_rgb(200, 180, 160)
+                        )?
+                    );
+                    // log(
+                    //     "INFO",
+                    //     format!(
+                    //         "Added light square at row {}, col {}: ( x:{}, y:{}, w:{}, h:{} ).",
+                    //         row,
+                    //         col,
+                    //         (board_x + 20.0) + (col as f32 * ((board_side_len - 40.0) / 8.0)),
+                    //         (board_y + 20.0) + (row as f32 * ((board_side_len - 40.0) / 8.0)),
+                    //         (board_side_len - 40.0) / 8.0,
+                    //         (board_side_len - 40.0) / 8.0
+                    //     )
+                    // );
+                }
+                col += 1;
+            }
+            row += 1;
+            // log("INFO", format!("Finished row, is now on row {}", row));
+        }
+
+        for mesh in meshes {
+            canvas.draw(&mesh, graphics::DrawParam::default());
+        }
+
         canvas.finish(ctx)
     }
 
+    fn resize_event(&mut self, _ctx: &mut Context, _width: f32, _height: f32) -> GameResult {
+        self.screen_coords = graphics::Rect::new(
+            0., 0., _width, _height
+        );
+        Ok(())
+    }
+
     fn quit_event(&mut self, _ctx: &mut Context) -> Result<bool, ggez::GameError> {
-        log("INFO", "The program should exit now.");
+        log("INFO", "The program will exit now.");
         // Ok(true) = keep running, Ok(false) = exit
         Ok(false)
     }
